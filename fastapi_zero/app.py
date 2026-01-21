e436a91f00b067cb7306c9751a923de46efddce1
mathkzm <matheusmiyashiro.mm@gmail.com>
2026-01-21 16:19:44 -0400

    Implementação das rotas CRUD

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,11 +1,18 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI(title='Primeira aplicação com FastAPI')
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -13,12 +20,64 @@ def read_root():
     return {'message': 'Olá, mundo!'}
 
 
-@app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
-def hello():
-    return """
-    <html>
-        <body>
-            <h1>Hello, world!</h1>
-        </body>
-    </html>
-    """
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=len(database) + 1,
+    )
+
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=user_id,
+    )
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found!'
+        )
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found!'
+        )
+    return database.pop(user_id - 1)
+
+
+# Exercicio da Aula 02
+
+# @app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+# def hello():
+#     return """
+#     <html>
+#         <body>
+#             <h1>Hello, world!</h1>
+#         </body>
+#     </html>
+#     """
