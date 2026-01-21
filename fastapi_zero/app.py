cba640c6f06f0739edcf79dc8c7aa693ecedb79a
mathkzm <matheusmiyashiro.mm@gmail.com>
2026-01-21 14:40:41 -0400

    Implementação da rota GET /hello, que retorna um HTML (Exercício da aula 02)

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,24 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='Primeira aplicação com FastAPI')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, mundo!'}
+
+
+@app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello():
+    return """
+    <html>
+        <body>
+            <h1>Hello, world!</h1>
+        </body>
+    </html>
+    """
