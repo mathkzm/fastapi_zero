c635ed98ca71c60098b86de016a2d1d834309b4d
mathkzm <matheusmiyashiro.mm@gmail.com>
2026-01-22 16:09:50 -0400

    Arquivo de testes do DB

---
+++ tests/test_db.py
@@ -0,0 +1,24 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fastapi_zero.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(username='test', email='test@test', password='secret')
+
+        session.add(new_user)
+        session.commit()  # Transmite as alterações para o banco de dados
+
+        # Tudo que vem do banco é convertido para um objeto Python
+        user = session.scalar(select(User).where(User.username == 'test'))
+
+        assert asdict(user) == {
+            'id': 1,
+            'username': 'test',
+            'email': 'test@test',
+            'password': 'secret',
+            'created_at': time,
+        }
