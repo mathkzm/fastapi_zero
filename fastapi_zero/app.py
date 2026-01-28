2f581244007ed868a7adac0d87e2b4bfb6b8b9ba
mathkzm <matheusmiyashiro.mm@gmail.com>
2026-01-28 16:22:23 -0400

    Implementação dos arquivos com base na autenticação

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,16 +1,25 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
 from fastapi_zero.database import get_session
 from fastapi_zero.models import User
 from fastapi_zero.schemas import (
     Message,
+    Token,
     UserList,
     UserPublic,
     UserSchema,
+)
+from fastapi_zero.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
 )
 
 app = FastAPI(title='Primeira aplicação com FastAPI')
@@ -47,7 +56,7 @@ def create_user(user: UserSchema, session=Depends(get_session)):
     db_user = User(
         username=user.username,
         email=user.email,
-        password=user.password,
+        password=get_password_hash(user.password),
     )
 
     session.add(db_user)
@@ -58,7 +67,12 @@ def create_user(user: UserSchema, session=Depends(get_session)):
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def read_users(offset: int = 0, limit: int = 10, session=Depends(get_session)):
+def read_users(
+    offset: int = 0,
+    limit: int = 10,
+    session=Depends(get_session),
+    current_user=Depends(get_current_user),
+):
     users = session.scalars(select(User).limit(limit).offset(offset))
     return {'users': users}
 
@@ -66,25 +80,28 @@ def read_users(offset: int = 0, limit: int = 10, session=Depends(get_session)):
 @app.put(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def update_user(user_id: int, user: UserSchema, session=Depends(get_session)):
+def update_user(
+    user_id: int,
+    user: UserSchema,
+    session=Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
 
-    user_db = session.scalar(select(User).where(User.id == user_id))
-
-    if not user_db:
+    if current_user.id != user_id:
         raise HTTPException(
-            detail='User Not Found', status_code=HTTPStatus.NOT_FOUND
+            status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
         )
 
     try:
-        user_db.email = user.email
-        user_db.username = user.username
-        user_db.password = user.password
+        current_user.email = user.email
+        current_user.username = user.username
+        current_user.password = get_password_hash(user.password)
 
-        session.add(user_db)
+        session.add(current_user)
         session.commit()
-        session.refresh(user_db)
+        session.refresh(current_user)
 
-        return user_db
+        return current_user
 
     except IntegrityError:
         raise HTTPException(
@@ -94,16 +111,18 @@ def update_user(user_id: int, user: UserSchema, session=Depends(get_session)):
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int, session=Depends(get_session)):
+def delete_user(
+    user_id: int,
+    session=Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
 
-    user_db = session.scalar(select(User).where(User.id == user_id))
-
-    if not user_db:
+    if current_user.id != user_id:
         raise HTTPException(
-            detail='User Not Found', status_code=HTTPStatus.NOT_FOUND
+            status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
         )
 
-    session.delete(user_db)
+    session.delete(current_user)
     session.commit()
 
     return {'message': 'User deleted'}
@@ -111,14 +130,42 @@ def delete_user(user_id: int, session=Depends(get_session)):
 
 # Exercício - Aula 03
 @app.get('/users/{user_id}', response_model=UserPublic)
-def read_users_id(user_id: int):
+def read_users_id(user_id: int, session: Session = Depends(get_session)):
 
-    if user_id < 1 or user_id > len(database):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found!'
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    # Como é um BD em uma lista, deve-se recuperar o usuário pelo índice.
-    return database[user_id - 1]
+
+    return db_user
+
+
+@app.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(
+        # O username é o email
+        select(User).where(User.email == form_data.username)
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail='Incorrect email or password',
+        )
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token({'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'Bearer'}
 
 
 # Exercicio da Aula 02
